99f0eceba9e6d1281da0d872450bf7f06ec13e9f
Stebbi <stefan.sigurdsson@sensa.is>
2026-05-05 11:15:21 +0000

    Fix menu route: use URL helper instead of route() helper

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -1,3 +1,3 @@
-<a href="{{ route('plugin', ['plugin' => 'Weathermap']) }}" class="nav-link">
+<a href="{{ url('/plugins/Weathermap') }}" class="nav-link">
     <i class="fa fa-map"></i> <span>Weathermap</span>
 </a>
